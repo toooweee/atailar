86b05a6ace1558f0091626991f438cd3de103147
Kosyrev143 <artemkosyrev93@gmail.com>
2025-10-04 23:37:39 +0500

    feat: (secret) add create

--- api/src/secrets/secrets.controller.ts
+++ api/src/secrets/secrets.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, UseGuards } from '@nestjs/common';
 import { SecretsService } from './secrets.service';
+import { CreateSecretDto } from './dtos/create-secret.dto';
+import { UserPayload } from '../auth/types/user.payload';
+import { Roles, User } from '@app/common';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Role } from '../../generated/prisma';
+import { ApprovedRequestGuard } from './guards/approved-request.guard';
 
 @Controller('secrets')
 export class SecretsController {
   constructor(private readonly secretsService: SecretsService) {}
+
+  @UseGuards(RolesGuard)
+  @Roles([Role.ADMIN])
+  @Post()
+  create(@Body() createSecretDto: CreateSecretDto, @User() user: UserPayload) {
+    return this.secretsService.createSecret(user.sub, createSecretDto);
+  }
+
+  @Get()
+  getAll() {
+    return this.secretsService.getAllSecrets();
+  }
+
+  @UseGuards(ApprovedRequestGuard)
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return this.secretsService.getOne(id);
+  }
 }
