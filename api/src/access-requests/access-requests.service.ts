0370e0e8cf1b62a4dfa5127ef25aa9b6b1c0d5da
Kosyrev143 <artemkosyrev93@gmail.com>
2025-10-04 17:20:00 +0500

    feat: (user) send credentials

--- api/src/access-requests/access-requests.service.ts
+++ api/src/access-requests/access-requests.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateAccessRequestDto } from './dtos/create-access-request.dto';
 
 @Injectable()
 export class AccessRequestsService {
   constructor(private readonly prisma: PrismaService) {
   }
 
-  create() {
+  create(dto: CreateAccessRequestDto) {
     // сохранить заявку в бд
     // отправить на email заявка с кнопками на одобрение и отклонение заявки
   }
 
-  findAll() {}
+  findAll() {
+    return this.prisma.accessRequest.findMany();
+  }
 
   approve(accessRequestId: string) {}
 
--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { EncryptionModule } from '../encryption/encryption.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [EncryptionModule],
+  imports: [
+    EncryptionModule,
+    MailModule
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
